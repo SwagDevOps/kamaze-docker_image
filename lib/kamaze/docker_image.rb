a18ee7f32f84b1fde21b15ccdb5c316e760bc48c
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-06-28 17:38:05 +0200

    docker_image uses readable_attrs

--- lib/kamaze/docker_image.rb
+++ lib/kamaze/docker_image.rb
@@ -54,16 +54,17 @@ class Kamaze::DockerImage
 
   autoload :Pathname, 'pathname'
   autoload :OpenStruct, 'ostruct'
+
   autoload :Runner, "#{__dir__}/docker_image/runner"
   autoload :Loader, "#{__dir__}/docker_image/loader"
   autoload :VERSION, "#{__dir__}/docker_image/version"
 
-  [:setup, :public_attrs].each do |req|
+  [:setup, :readable_attrs].each do |req|
     require_relative "#{__dir__}/docker_image/concern/#{req}"
   end
 
   include Concern::Setup
-  include Concern::PublicAttrs
+  include Concern::ReadableAttrs
 
   # @param [String] name
   def initialize
@@ -78,16 +79,19 @@ class Kamaze::DockerImage
     @name = @name.to_s
     @commands = Hash[@commands.map { |k, v| [k.to_sym, v] }]
     to_h.each { |k, v| instance_variable_set("@#{k}", v) }
+
+    @runner = Runner.new(self)
     tasks_load! if tasks_load?
   end
 
-  # @return [Hash]
-  def to_h
-    attrs = {}
-    public_attrs.each { |k| attrs[k] = self.public_send(k) }
-
-    attrs
+  # Denote image is running/started
+  #
+  # @return [Boolean]
+  def started?
+    runner.started?
   end
+
+  alias running? started?
 
   def to_s
     "#{name}:#{version}"
@@ -108,7 +112,7 @@ class Kamaze::DockerImage
     Pathname.new(@path)
   end
 
-  # @see Kamaze::DockerImage::Runner#actions
+  # @see Runner#actions
   def method_missing(method, *args, &block)
     if respond_to_missing?(method)
       return runner.public_send(method, *args, &block)
@@ -118,11 +122,9 @@ class Kamaze::DockerImage
   end
 
   def respond_to_missing?(method, include_private = false)
-    if runner.actions.include?(method.to_sym)
-      return runner.respond_to?(method, include_private)
-    end
+    flag = (runner&.actions).to_a.include?(method.to_sym)
 
-    super(method, include_private)
+    flag || super(method, include_private)
   end
 
   protected
@@ -137,15 +139,13 @@ class Kamaze::DockerImage
   attr_writer :exec_command
   attr_writer :commands
 
+  # @return [Runner]
+  attr_reader :runner
+
   # Load tasks
   def tasks_load!
     Loader.new(self).call
 
     self
-  end
-
-  # @return [Runner]
-  def runner
-    Runner.new(self)
   end
 end
