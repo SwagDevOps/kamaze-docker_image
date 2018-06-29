6b65a4763b273194b8770124e9499daeb7530dda
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-06-29 13:32:49 +0200

    setup (concern) self returns added on some methods

--- lib/kamaze/docker_image/concern/setup.rb
+++ lib/kamaze/docker_image/concern/setup.rb
@@ -19,37 +19,47 @@ module Kamaze::DockerImage::Concern::Setup
   # Setup
   #
   # @param [Array<Thread::Backtrace::Location>] locations
+  # @return [self]
   def setup(locations, &block)
-    setup_defaults(locations)
-    setup_block(&block) if block
+    setup_defaults(locations).tap do
+      setup_block(&block)
 
-    @name = @name.to_s
-    @commands = Hash[@commands.map { |k, v| [k.to_sym, v] }]
-    to_h.each { |k, v| instance_variable_set("@#{k}", v) }
+      @name = @name.to_s
+      @commands = Hash[@commands.map { |k, v| [k.to_sym, v] }]
+
+      to_h.each { |k, v| instance_variable_set("@#{k}", v) }
+    end
   end
+
+  # rubocop:disable Metrics/MethodLength
 
   # Setup atttributes with default values
   #
   # @param [Array<Thread::Backtrace::Location>] locations
+  # @return [self]
   def setup_defaults(locations)
-    @name = nil
-    @version = 'latest'
+    self.tap do
+      @name = nil
+      @version = 'latest'
 
-    @path = Pathname.new('.')
-    @verbose = $stdout.tty? && $stderr.tty?
-    @tasks_load = true
-    @tasks_ns = nil
-    @run_as = called_from(locations).dirname.basename.to_s
-    @docker_bin = 'docker'
-    @exec_command = 'bash'
-    @commands = default_commands
+      @path = Pathname.new('.')
+      @verbose = $stdout.tty? && $stderr.tty?
+      @tasks_load = true
+      @tasks_ns = nil
+      @run_as = called_from(locations).dirname.basename.to_s
+      @docker_bin = 'docker'
+      @exec_command = 'bash'
+      @commands = default_commands
+    end
   end
 
-  # @yield [OpenStruct] Config used to setup instance
+  # rubocop:enable Metrics/MethodLength
+
+  # @yield [OpenStruct] config used to setup instance
   # @return [OpenStruct]
-  def setup_block(&_block)
+  def setup_block
     OpenStruct.new(self.respond_to?(:to_h) ? to_h : {}).tap do |s|
-      yield(s)
+      yield(s) if block_given?
 
       s.freeze
       s.to_h.each { |k, v| setup_attr(k, v) }
