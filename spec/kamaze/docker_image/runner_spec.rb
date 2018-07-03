17c3b8bd6299d6042c3b8d47f86715c9963b8e8d
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-07-03 16:54:57 +0200

    runner (spec) public instance methods added

--- spec/kamaze/docker_image/runner_spec.rb
+++ spec/kamaze/docker_image/runner_spec.rb
@@ -8,3 +8,25 @@ describe Kamaze::DockerImage::Runner, :runner do
     expect(described_class).to respond_to(:new).with(1).arguments
   end
 end
+
+describe Kamaze::DockerImage::Runner, :runner do
+  let(:subject) { sham!(:runner).maker.call(:base) }
+
+  # instance methods
+  it do
+    # actions
+    expect(subject).to respond_to(:build).with(0).arguments
+    expect(subject).to respond_to(:run).with(0).arguments
+    expect(subject).to respond_to(:run).with(1).arguments
+    expect(subject).to respond_to(:exec).with(0).arguments
+    expect(subject).to respond_to(:exec).with(1).arguments
+    expect(subject).to respond_to(:start).with(0).arguments
+    expect(subject).to respond_to(:run).with(0).arguments
+    expect(subject).to respond_to(:stop).with(0).arguments
+    expect(subject).to respond_to(:restart).with(0).arguments
+    # misc
+    expect(subject).to respond_to(:actions).with(0).arguments
+    expect(subject).to respond_to(:started?).with(0).arguments
+    expect(subject).to respond_to(:running?).with(0).arguments
+  end
+end
