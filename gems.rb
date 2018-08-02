418bb396850bde8b385e646e91868df52c29823b
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-02 20:23:10 +0200

    gems refactor

--- gems.rb
+++ gems.rb
@@ -14,21 +14,21 @@ group :development do
   gem 'listen', '~> 3.1'
   gem 'rubocop', '~> 0.56'
   gem 'sys-proc', '~> 1.1'
+end
 
-  group :repl do
-    gem 'interesting_methods', '~> 0.1'
-    gem 'pry', '~> 0.11'
-    gem 'pry-coolline', '~> 0.2'
-  end
+group :test do
+  gem 'rspec', '~> 3.6'
+  gem 'sham', '~> 2.0'
+end
 
-  group :doc do
-    gem 'github-markup', '~> 2.0'
-    gem 'redcarpet', '~> 3.4'
-    gem 'yard', '~> 0.9'
-  end
+group :doc do
+  gem 'github-markup', '~> 2.0'
+  gem 'redcarpet', '~> 3.4'
+  gem 'yard', '~> 0.9'
+end
 
-  group :test do
-    gem 'rspec', '~> 3.6'
-    gem 'sham', '~> 2.0'
-  end
+group :repl do
+  gem 'interesting_methods', '~> 0.1'
+  gem 'pry', '~> 0.11'
+  gem 'pry-coolline', '~> 0.2'
 end
