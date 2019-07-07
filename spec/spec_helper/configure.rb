e02f5884cfa55e473ca4b6291ebd1166decf82aa
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-07-07 17:24:06 +0200

    spec_helper (spec) minor changes

--- spec/spec_helper/configure.rb
+++ spec/spec_helper/configure.rb
@@ -4,12 +4,6 @@ require 'pathname'
 require 'sham'
 require_relative 'local'
 
-if Gem::Specification.find_all_by_name('sys-proc').any?
-  require 'sys/proc'
-
-  Sys::Proc.progname = 'rspec'
-end
-
 Sham::Config.activate!(Pathname.new(__dir__).join('..').realpath)
 
 Object.class_eval { include Local }
