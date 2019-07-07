bad2a4f402dacfb0d27f5f4c47b467d2d9f441dc
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-07-07 17:40:50 +0200

    loader/tasks (lib) minor changes

--- lib/kamaze/docker_image/loader/tasks.rb
+++ lib/kamaze/docker_image/loader/tasks.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-autoload(:YAML, 'yaml')
-
 # Copyright (C) 2017-2018 Dimitri Arrigoni <dimitri@arrigoni.me>
 # License GPLv3+: GNU GPL version 3 or later
 # <http://www.gnu.org/licenses/gpl.html>.
 # This is free software: you are free to change and redistribute it.
 # There is NO WARRANTY, to the extent permitted by law.
+
+autoload(:YAML, 'yaml')
 
 # Make task name using namespace (from image)
 #
