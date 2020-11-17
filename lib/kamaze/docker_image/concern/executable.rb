5173a0316dd46cfc0ea74081304d008448af9dd9
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-11-17 19:20:17 +0100

    executable (concern) introduced

---
+++ lib/kamaze/docker_image/concern/executable.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2017-2020 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
+require_relative '../concern'
+
+# PProvide method to retrieve path to docker executable.
+module Kamaze::DockerImage::Concern::Executable
+  autoload(:Cliver, 'cliver')
+
+  protected
+
+  # Get executable
+  #
+  # @raise [Cliver::Dependency::NotFound]
+  # @return [String]
+  def executable
+    Cliver.detect!(:docker).freeze
+  end
+end
