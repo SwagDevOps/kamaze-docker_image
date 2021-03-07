08ff4c347f4332a489637e2d27ab262f45176e4d
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-03-07 19:47:31 +0100

    containers (concern) major changes and improvements

--- lib/kamaze/docker_image/concern/containers.rb
+++ lib/kamaze/docker_image/concern/containers.rb
@@ -17,12 +17,14 @@ module Kamaze::DockerImage::Concern::Containers
 
   protected
 
+  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+
   # List containers.
   #
   # Containers are indexed by name.
   #
   # @return [Hash{String => Hash}]
-  def containers(all: true) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/PerceivedComplexity
+  def containers(all: true)
     [(config[:docker_bin] || executable).to_s].map(&:freeze).freeze.yield_self do |cmd|
       cmd.dup.concat(['ps', (all ? '-a' : nil), '--format', '{{json .}}']).yield_self do |command|
         [].tap do |items|
@@ -30,18 +32,44 @@ module Kamaze::DockerImage::Concern::Containers
             stdout.lines.each { |line| items.push(JSON.parse(line)) } if status.success?
           end
         end
-      end.map do |h|
-        cmd.dup.concat(['inspect', '--format', '{{json .}}', h.fetch('ID')]).yield_self do |command|
-          Open3.capture3(*command.compact).yield_self do |stdout, _, status|
-            h.tap do
-              h.singleton_class.tap do |klass|
-                klass.__send__(:define_method, :info) { (status.success? ? JSON.parse(stdout) : nil).to_h }
-                klass.__send__(:define_method, :running?) { info['State'].to_h['Status'] == 'running' }
-              end
-            end
-          end
-        end
-      end.map { |h| [h.info['Name'].gsub(%r{^/*}, '').to_s, h] }.to_h
+      end.map { |h| containers_maker.call(h) }.map { |h| [h.info['Name'].gsub(%r{^/*}, '').to_s, h] }.to_h
     end
   end
+
+  # Add some methods on retrieved info.
+  #
+  # @api private
+  #
+  # @return [Proc}
+  def containers_maker
+    # @type [Hash{String => Object}] given_data
+    lambda do |given_data|
+      command = [(config[:docker_bin] || executable).to_s]
+                .map(&:freeze)
+                .concat(['inspect', '--format', '{{json .}}', given_data.fetch('ID')])
+
+      given_data.dup.tap do |result|
+        result.singleton_class.tap do |klass|
+          :inspection.tap do |method_name|
+            klass.__send__(:define_method, method_name) do
+              -> { Struct.new(:stdout, :stderr, :status).new(*Open3.capture3(*command.compact)) }
+            end
+            klass.__send__(:protected, method_name)
+          end
+
+          klass.__send__(:define_method, :info) do
+            inspection.call.yield_self do |v|
+              (v.status.success? ? JSON.parse(v.stdout) : nil).to_h
+            end
+          end
+
+          klass.__send__(:define_method, :running?) do
+            self.info['State'].to_h['Status'] == 'running'
+          end
+        end
+      end.freeze
+    end
+  end
+
+  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 end
